38faa7261ad36decf6d211a8732305dd0da83144
ayr <vj_1@list.ru>
2023-12-07 18:18:26 +0300

    HW_21 add @JsonIgnoreProperties

--- src/test/java/models/UserBodyResponseModel.java
+++ src/test/java/models/UserBodyResponseModel.java
@@ -1,9 +1,10 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties; //Для игнора неиспользуемых параметров
 import lombok.Data;
 
 @Data
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserBodyResponseModel {
     String name, job;
 }
